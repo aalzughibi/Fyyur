c0f551883274b54d3af8bf3fb4971d967a835e3f
Abdullah <aalzughibi2@gmail.com>
2020-06-24 03:17:47 +0300

    full version

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\abdullah\desktop\fsnd-master\projects\01_fyyur\starter_code\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
